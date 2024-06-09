c80b6a5d755898a6a0522f2ceef1a10872fb8bfe
Axelle Tanoto <axelle.tanoto@gmail.com>
2024-06-10 02:04:46 +0700

    ubah array

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 import streamlit as st
 import pickle
+import numpy as np
 
 diabetes_model = pickle.load(open('dia_mod.pkl', 'rb'))
 
 heart_model = pickle.load(open('heart_mod.pkl', 'rb'))
 
 def predict_diabetes(features):
+    features = np.array(features).reshape(1, -1)
     prediction = diabetes_model.predict(features)
     return prediction
 
 def predict_heart_disease(features):
+    features = np.array(features).reshape(1, -1)
     prediction = heart_model.predict(features)
     return prediction
 
@@ -49,9 +52,24 @@ def main():
     PhysHlth = st.slider('Physical illness in past 30 days', 0.0, 30.0)
     DiffWalk = st.radio('Do you have serious difficulty walking or climbing stairs?', ['No', 'Yes'])
 
-    diabetes_features = [Age, Sex, HighBP, HighChol, CholCheck, BMI, Smoker, Stroke, PhysActivity, Fruits, Veggies, HvyAlcoholConsump, AnyHealthcare, NoDocbcCost, GenHlth, MentHlth, PhysHlth, DiffWalk]
+    sex_mapping = {'Female': 0, 'Male': 1}
+    high_bp_mapping = {'No': 0, 'Yes': 1}
+    high_chol_mapping = {'No': 0, 'Yes': 1}
+    chol_check_mapping = {'No': 0, 'Yes': 1}
+    smoker_mapping = {'No': 0, 'Yes': 1}
+    stroke_mapping = {'No': 0, 'Yes': 1}
+    phys_activity_mapping = {'No': 0, 'Yes': 1}
+    fruits_mapping = {'No': 0, 'Yes': 1}
+    veggies_mapping = {'No': 0, 'Yes': 1}
+    hvy_alcohol_mapping = {'No': 0, 'Yes': 1}
+    healthcare_mapping = {'No': 0, 'Yes': 1}
+    no_doc_cost_mapping = {'No': 0, 'Yes': 1}
+    gen_hlth_mapping = {'Excellent': 0, 'Very Good': 1, 'Good': 2, 'Fair': 3, 'Poor': 4}
+    diff_walk_mapping = {'No': 0, 'Yes': 1}
 
-    heart_features = [Age, Sex, HighBP, HighChol, CholCheck, BMI, Smoker, Stroke, PhysActivity, Fruits, Veggies, HvyAlcoholConsump, AnyHealthcare, NoDocbcCost, GenHlth, MentHlth, PhysHlth, DiffWalk]
+    diabetes_features = [Age, sex_mapping[Sex], high_bp_mapping[HighBP], high_chol_mapping[HighChol], chol_check_mapping[CholCheck], BMI, smoker_mapping[Smoker], stroke_mapping[Stroke], phys_activity_mapping[PhysActivity], fruits_mapping[Fruits], veggies_mapping[Veggies], hvy_alcohol_mapping[HvyAlcoholConsump], healthcare_mapping[AnyHealthcare], no_doc_cost_mapping[NoDocbcCost], gen_hlth_mapping[GenHlth], MentHlth, PhysHlth, diff_walk_mapping[DiffWalk]]
+
+    heart_features = [Age, sex_mapping[Sex], high_bp_mapping[HighBP], high_chol_mapping[HighChol], chol_check_mapping[CholCheck], BMI, smoker_mapping[Smoker], stroke_mapping[Stroke], phys_activity_mapping[PhysActivity], fruits_mapping[Fruits], veggies_mapping[Veggies], hvy_alcohol_mapping[HvyAlcoholConsump], healthcare_mapping[AnyHealthcare], no_doc_cost_mapping[NoDocbcCost], gen_hlth_mapping[GenHlth], MentHlth, PhysHlth, diff_walk_mapping[DiffWalk]]
     
     if st.button('Predict Diabetes'):
         diabetes_prediction = predict_diabetes(diabetes_features)
