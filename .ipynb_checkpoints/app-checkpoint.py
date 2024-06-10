b495057f84933817acb10cab446a652e47274058
Axelle Tanoto <axelle.tanoto@gmail.com>
2024-06-10 20:38:17 +0700

    kolom

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 
 diabetes_model = pickle.load(open('dia_mod.pkl', 'rb'))
+
 heart_model = pickle.load(open('heart_mod.pkl', 'rb'))
 
 def predict_diabetes(features):
@@ -17,89 +18,79 @@ def predict_heart_disease(features):
 def main():
     st.title('Diabetes and Heart Disease Prediction')
     age_mapping = {
-        '18-24': 1, '25-29': 2, '30-34': 3, '35-39': 4, '40-44': 5,
-        '45-49': 6, '50-54': 7, '55-59': 8, '60-64': 9, '65-69': 10,
-        '70-74': 11, '75-79': 12, '80 or older': 13
+        '18-24': 1,
+        '25-29': 2,
+        '30-34': 3,
+        '35-39': 4,
+        '40-44': 5,
+        '45-49': 6,
+        '50-54': 7,
+        '55-59': 8,
+        '60-64': 9,
+        '65-69': 10,
+        '70-74': 11,
+        '75-79': 12,
+        '80 or older': 13
     }
-
-    # CSS style to balance the column width
-    st.markdown(
-        """
-        <style>
-        .equalSpace > div {
-            flex: 1;
-            margin-right: 5px;
-            margin-left: 5px;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-
-    col1, col2, col3, col4 = st.columns(4)
-    
+    col1, col2 = st.columns(2)
     with col1:
-        st.header('Age')
-        Age = st.radio('Select your age range:', list(age_mapping.keys()))
-
-        st.header('Sex/Gender')
-        Sex = st.radio('Select your gender:', ['Female', 'Male'])
-
-        st.header('High Blood Pressure')
-        HighBP = st.radio('Do you have high blood pressure?', ['No', 'Yes'])
-
+        st.header('Age (1st Half)')
+        Age = st.radio('Select your age range:', list(age_mapping.keys())[:len(age_mapping)//2])
+        
     with col2:
-        st.header('High Cholesterol')
-        HighChol = st.radio('Do you have high cholesterol?', ['No', 'Yes'])
+        st.header('Age (2nd Half)')
+        Age = st.radio('Select your age range:', list(age_mapping.keys())[len(age_mapping)//2:])
 
-        st.header('Cholesterol Check')
-        CholCheck = st.radio('Have you had a cholesterol check in the last 5 years?', ['No', 'Yes'])
+    st.header('Sex/Gender')
+    Sex = st.radio('Select your gender:', ['Female', 'Male'])
 
-        st.header("Body Mass Index (BMI)")
-        BMI = st.slider('Select your BMI:', 12.0, 98.0)
+    st.header('High Blood Pressure')
+    HighBP = st.radio('Do you have high blood pressure?', ['No', 'Yes'])
 
-    with col3:
-        st.header("Smoker")
-        Smoker = st.radio('Are you a smoker?', ['No', 'Yes'])
+    st.header('High Cholestrol')
+    HighChol = st.radio('Do you have high choloestrol?', ['No', 'Yes'])
 
-        st.header("Stroke")
-        Stroke = st.radio('Have you had a stroke?', ['No', 'Yes'])
+    st.header('Cholestrol Check')
+    CholCheck = st.radio('Have you had a cholesterol check in the last 5 years?', ['No', 'Yes'])
 
-        st.header("Physical Activity")
-        PhysActivity = st.radio('Have you engaged in physical activity in the past 30 days?', ['No', 'Yes'])
+    st.header("Body Mass Index (BMI)")
+    BMI = st.slider('Select your BMI:', 12.0, 98.0)
 
-    with col4:
-        st.header("Fruits Consumption")
-        Fruits = st.radio('Do you consume fruits 1 or more times per day?', ['No', 'Yes'])
+    st.header("Smoker")
+    Smoker = st.radio('Are you a smoker?', ['No', 'Yes'])
 
-        st.header("Vegetables Consumption")
-        Veggies = st.radio('Do you consume vegetables 1 or more times per day?', ['No', 'Yes'])
+    st.header("Stroke")
+    Stroke = st.radio('Have you had a stroke?', ['No', 'Yes'])
 
-        st.header("Heavy Alcohol Consumption")
-        HvyAlcoholConsump = st.radio('Do you engage in heavy alcohol consumption?', ['No', 'Yes'])
+    st.header("Physical Activity")
+    PhysActivity = st.radio('Have you engaged in physical activity in the past 30 days?', ['No', 'Yes'])
 
-        st.header("Health Care Coverage")
-        AnyHealthcare = st.radio('Do you have any kind of health care coverage?', ['No', 'Yes'])
+    st.header("Fruits Consumption")
+    Fruits = st.radio('Do you consume fruits 1 or more times per day?', ['No', 'Yes'])
 
-    col5, col6, col7, col8 = st.columns(4)
+    st.header("Vegetables Consumption")
+    Veggies = st.radio('Do you consume vegetables 1 or more times per day?', ['No', 'Yes'])
     
-    with col5:
-        st.header("Unable to See a Doctor Due to Cost")
-        NoDocbcCost = st.radio('Have you been unable to see a doctor due to cost in the past 12 months?', ['No', 'Yes'])
+    st.header("Heavy Alcohol Consumption")
+    HvyAlcoholConsump = st.radio('Do you engage in heavy alcohol consumption? [Adult Men > 14 drinks per week][Adult Women > 7 drinks per week],', ['No', 'Yes'])
 
-        st.header("General Health")
-        GenHlth = st.radio('Rate your general health:', ['Excellent', 'Very Good', 'Good', 'Fair', 'Poor'])
+    st.header("Health Care Coverage")
+    AnyHealthcare = st.radio('Do you have any kind of health care coverage?', ['No', 'Yes'])
 
-    with col6:
-        st.header("Days of Poor Mental Health")
-        MentHlth = st.slider('How many days of poor mental health have you experienced in the past 30 days?', 0.0, 30.0)
+    st.header("Unable to See a Doctor Due to Cost")
+    NoDocbcCost = st.radio('Have you been unable to see a doctor due to cost in the past 12 months?', ['No', 'Yes'])
+    
+    st.header("General Health")
+    GenHlth = st.radio('Rate your general health:', ['Excellent', 'Very Good', 'Good', 'Fair', 'Poor'])
+    
+    st.header("Days of Poor Mental Health")
+    MentHlth = st.slider('How many days of poor mental health have you experienced in the past 30 days?', 0.0, 30.0)
 
-        st.header("Physical Illness")
-        PhysHlth = st.slider('How many days have you experienced physical illness in the past 30 days?', 0.0, 30.0)
+    st.header("Physical Illness")
+    PhysHlth = st.slider('How many days have you experienced physical illness in the past 30 days?', 0.0, 30.0)
 
-    with col7:
-        st.header("Difficulty Walking or Climbing Stairs")
-        DiffWalk = st.radio('Do you have serious difficulty walking or climbing stairs?', ['No', 'Yes'])
+    st.header("Difficulty Walking or Climbing Stairs")
+    DiffWalk = st.radio('Do you have serious difficulty walking or climbing stairs?', ['No', 'Yes'])
 
     sex_mapping = {'Female': 0, 'Male': 1}
     high_bp_mapping = {'No': 0, 'Yes': 1}
